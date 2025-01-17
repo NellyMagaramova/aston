e8b857d6d51d9556c1cf6b1735c9fe6e4d2ae5ea
NellyMagaramova <jasmin_oir@mail.ru>
2025-01-17 10:00:34 +0300

    to review

--- src/main/java/org/test/Main.java
+++ src/main/java/org/test/Main.java
@@ -1,17 +1,45 @@
 package org.test;
 
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public void printThreeWords(){
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        printThreeWords();
+        ckeckSumSign();
+        printColor();
+        compareNumbers();
+        boolean result =  func5(5, 10);
+        System.out.println("result = "+ result);
+        func6(5);
+        result = main.func7(0);
+        System.out.println("result = "+ result);
+        func8("Hello", 3);
+        result = func9(2000);
+        System.out.println("result = "+ result);
+        func10();
+        func11();
+        func12();
+        func13(10);
+        func14(3, 2);
+    }
+
+    public static void printThreeWords(){
         String a = "Orange";
         String b = "Banana";
         String c = "Apple";
-        System.out.println(a + '\n'+ b + '\n' + c);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
     }
 
-    public void ckeckSumSign(){
+    public static void ckeckSumSign(){
         int a, b;
         a = 8;
         b = 9;
@@ -20,14 +48,14 @@ public class Main {
         else System.out.println("Сумма отрицательная");
     }
 
-    public void printColor(){
+    public static void printColor(){
         int value = 1;
         if(value<=0)  System.out.println("Красный");
         if(value>0 && value <= 100)  System.out.println("Желтый");
         if(value >= 100)  System.out.println("Зеленый");
     }
 
-    public void compareNumbers(){
+    public static void compareNumbers(){
         int a,b;
         a = 5;
         b = 7;
@@ -35,36 +63,41 @@ public class Main {
         else System.out.println("a<b");
     }
 
-    public boolean func1(int a, int b){
+    public static  boolean func5(int a, int b){
+        System.out.println("func 5  ");
         int summ = a+b;
         if(summ>=10 && summ <=20) return true;
         return false;
     }
 
-    public void func2(int a) {
+    public static void func6(int a) {
+        System.out.println("func 6  ");
         if(a>=0) System.out.println("число положительное");
         else System.out.println("число отрицательное");
     }
 
-    public boolean func3(int a) {
+    public static boolean func7(int a) {
+        System.out.println("func 7  ");
         if(a<0) return true;
         return false;
     }
 
-    public void func4(String str, int count){
+    public static void func8(String str, int count){
+        System.out.println("func 8  ");
         for(int i = 0; i<count; i++){
             System.out.println(str);
         }
     }
 
-public boolean func5(int value) {
-    if (value % 4 == 0 && value % 100 != 0) {
-        return true;
+    public static boolean func9(int value) {
+        System.out.println("func 9  ");
+        if (value % 4 == 0 && value % 100 != 0) {
+            return true;
+        }
+        return false;
     }
-    return false;
-}
-//
-    public  void func6(){
+
+    public static  void func10(){
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for(int i = 0; i<arr.length; i++)
         {
@@ -72,72 +105,50 @@ public boolean func5(int value) {
                 case 0: arr[i] = 1; break;
                 case 1: arr[i] = 0; break;
             }
-            System.out.println(arr[i]);
         }
+        System.out.println("func 10 = "+ Arrays.toString(arr));
     }
 
-    public void func7(){
+    public static void func11(){
         int[] arr = new int[100];
         for(int i = 0; i<arr.length; i++){
             arr[i] = i+1;
-            System.out.println(arr[i]);
         }
+        System.out.println("func 11 = "+ Arrays.toString(arr));
     }
 
-    public  void func8(){
+    public static  void func12(){
         int[] arr= {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for(int i = 0; i< arr.length; i++)
         {
             if(arr[i]<6)
                 arr[i] = arr[i] *2;
-            System.out.println(arr[i]);
         }
+        System.out.println("func 12 = "+ Arrays.toString(arr));
     }
 
-    public void func9(int count){
-        int[][] array = new int[count][count];
+    public static void func13(int count){
+        int[][] arr = new int[count][count];
+        System.out.println("func 13 ");
 
         for (int i = 1; i < count; i++) {
             for (int j = 1; j < count; j++) {
                 if (i == j) {
-                    array[i][j]=1;
+                    arr[i][j]=1;
                 }else {
-                    array[i][j]=0;
+                    arr[i][j]=0;
                 }
-                System.out.print(array[i][j] + " ");
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
     }
 
-    public void func10(int len, int initialValue){
+    public static void func14(int len, int initialValue){
         int[] arr =  new int[len];
         for(int i = 0; i<arr.length; i++){
             arr[i] = initialValue;
-            System.out.print(arr[i]+" ");
         }
-    }
-
-    public static void main(String[] args) {
-        Main main = new Main();
-
-        main.printThreeWords();
-        main.ckeckSumSign();
-        main.printColor();
-        main.compareNumbers();
-
-        boolean result =  main.func1(5, 10);
-        System.out.println("result = "+ result);
-        main.func2(5);
-        result = main.func3(0);
-        System.out.println("result = "+ result);
-        main.func4("Hello", 3);
-        result = main.func5(2000);
-        System.out.println("result = "+ result);
-        main.func6();
-        main.func7();
-        main.func8();
-        main.func9(10);
-        main.func10(3, 2);
+        System.out.println("func 14 = "+ Arrays.toString(arr));
     }
 }
