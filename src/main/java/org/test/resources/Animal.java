b8f4144f5033db2d634a73c3e9538efc37b07590
NellyMagaramova <jasmin_oir@mail.ru>
2025-01-24 21:13:24 +0300

    to review

--- src/main/java/org/test/resources/Animal.java
+++ src/main/java/org/test/resources/Animal.java
@@ -1,23 +1,10 @@
 package org.test.resources;
 
 public class Animal {
-//    private int maxFood;
-//
-//    public int getMaxFood() {
-//        return maxFood;
-//    }
-//
-//    public void setMaxFood(int maxFood) {
-//        this.maxFood = maxFood;
-//    }
 
     public static int count;
 
     public  void run(int m){};
     public  void swim(int m){};
     public Animal(){count+=1;}
-
-//    public Animal() {
-//        this.maxFood = maxFood;
-//    }
 }
