088ef2f7054cdd456b01771dcd805ec8f8d35f74
NellyMagaramova <jasmin_oir@mail.ru>
2025-01-25 09:43:34 +0300

    to review

--- src/main/java/org/test/Main.java
+++ src/main/java/org/test/Main.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
     public static void main(String[] args) {
         Cat cat1 = new Cat();
         cat1.run(10);
