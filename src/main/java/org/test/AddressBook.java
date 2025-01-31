6ece43aa9dcbd80ba5654722aa2fa12af78a799a
NellyMagaramova <jasmin_oir@mail.ru>
2025-01-31 21:26:59 +0300

    to review

---
+++ src/main/java/org/test/AddressBook.java
@@ -0,0 +1,41 @@
+package org.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddressBook {
+    private Map<Long, String> phoneMap = new HashMap();
+
+    public Map<Long, String> getPhoneMap() {
+        return phoneMap;
+    }
+
+    public void setPhoneMap(Map<Long, String> phoneMap) {
+        this.phoneMap = phoneMap;
+    }
+
+    public void add(Long number, String fio) {
+        phoneMap.put(number, fio);
+    }
+
+    public void get(String fio) {
+        for (Map.Entry<Long, String> entry : phoneMap.entrySet()) {
+            if (entry.getValue().equals(fio)) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        addressBook.add(79202520308L, "Nelly Magaramova");
+        addressBook.add(79200281319L, "Vera Magaramova");
+        addressBook.add(79200270953L, "Aslan Magaramov");
+        addressBook.add(79200270954L, "Aslan Magaramov");
+
+        addressBook.get("Aslan Magaramov");
+
+
+    }
+
+}
--- src/main/java/org/test/Main.java
+++ src/main/java/org/test/Main.java
@@ -1,11 +1,30 @@
 package org.test;
 
-import java.util.Arrays;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String[] mass = new String[]{"abc", "abe", "abc"};
+        List<String> unqiueList = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
 
+        for (String s : mass) {
+            if (map.get(s) != null) {
+                map.put(s, map.get(s) + 1);
+            } else {
+                map.put(s, 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                unqiueList.add(entry.getKey());
+            }
+            System.out.println("слово " + entry.getKey() + " встречается " + entry.getValue() + " раз");
+        }
+        System.out.println("уникальные значения " + unqiueList);
     }
+
 }
