804fcc1e6bfe0f98b3f4c1e52e93bedb6f9f32ae
NellyMagaramova <jasmin_oir@mail.ru>
2025-02-02 14:58:11 +0300

    to review

--- src/main/java/org/test/Main.java
+++ src/main/java/org/test/Main.java
@@ -6,7 +6,8 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String[] mass = new String[]{"abc", "abe", "abc"};
+        String[] mass = new String[]{"Nelly", "Aslan", "Vera", "Vera", "Nelly", "Vasya", "cat", "dog", "cat", "home",
+            "father", "mother", "sister", "sister", "friend", "boyfriend", "note", "computer", "book", "book"};
         List<String> unqiueList = new ArrayList<>();
         HashMap<String, Integer> map = new HashMap<>();
 
