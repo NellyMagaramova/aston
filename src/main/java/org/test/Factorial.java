fe714a0889322024776a02684d2ef7562d8772c6
NellyMagaramova <jasmin_oir@mail.ru>
2025-02-06 17:28:52 +0300

    to review

---
+++ src/main/java/org/test/Factorial.java
@@ -0,0 +1,12 @@
+package org.test;
+
+public class Factorial {
+
+    public int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
