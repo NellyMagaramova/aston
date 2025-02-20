82e8155289a29bfa2b30ee172c3900534faad510
NellyMagaramova <jasmin_oir@mail.ru>
2025-02-20 11:58:31 +0300

    to review

--- src/test/java/test/TestPayment.java
+++ src/test/java/test/TestPayment.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class TestPayment {
     private WebDriver driver;
     private MtsPaymentPage paymentPage;
@@ -15,6 +17,7 @@ public class TestPayment {
     public void setup(){
         driver = new ChromeDriver();
         paymentPage = new MtsPaymentPage(driver);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @AfterEach
